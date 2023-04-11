a4241667a9f431b7da5a0fefff67a3245b6a9e72
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-04-12 02:18:17 +0300

    Added Grid to weather info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,28 @@
-import HeaderInfo from "@/components/UI/HeaderInfo";
-import Weekly from "@/components/Weekly";
-import DailyTemp from "@/components/DailyTemp";
+import HeaderInfo from '@/components/UI/HeaderInfo';
+import Weekly from '@/components/Weekly';
+import DailyTemp from '@/components/DailyTemp';
+import HumidityBox from '@/components/UI/InfoBox';
+import InfoBox from '@/components/UI/InfoBox';
 
-
-export default function Index(){
-  return(
-      <div className={"flex flex-col"}>
-          <div className={"flex flex-col w-full gap-12 items-center justify-center"}>
-              <HeaderInfo icon={"sunny.svg"} lowTemp={"10"} highTemp={"20"} temp={"12"} city={"Bacau"}/>
-          </div>
-          <div className={"mt-12"}>
-              <DailyTemp/>
-          </div>
-          <div className={"mt-3"}>
-            <Weekly/>
-          </div>
-      </div>
-  )
+export default function Index() {
+	return (
+		<div className={'flex flex-col'}>
+			<div className={'flex flex-col w-full gap-12 items-center justify-center'}>
+				<HeaderInfo icon={'sunny.svg'} lowTemp={'10'} highTemp={'20'} temp={'12'} city={'Bacau'} />
+			</div>
+			<div className={'mt-12'}>
+				<DailyTemp />
+			</div>
+			<div className={'grid grid-cols-4 grid-rows-4 grid-flow-dense gap-5 mt-3'}>
+				<div className={"col-span-2 row-span-3"}>
+					<Weekly />
+				</div>
+					<InfoBox icon={'sunrise.svg'} data={'09:00PM'} title={'Sun Rise'} />
+					<InfoBox icon={'humidity.svg'} data={'50%'} title={'Humidity'} />
+					<InfoBox icon={'windy.svg'} data={'14km/h'} title={'Wind'} />
+					<InfoBox icon={'sunny.svg'} data={'100'} title={'UV Index'} />
+					<InfoBox icon={'umbrella.svg'} data={'10%'} title={'Precipitation'} />
+			</div>
+		</div>
+	);
 }
