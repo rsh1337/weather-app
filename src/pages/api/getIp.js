c26a3eb00f0603960010ea5e0d61ab8e1328caa2
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-04-14 00:20:13 +0300

    Add geoip to packages

--- src/pages/api/getIp.js
+++
@@ -1,8 +0,0 @@
-import geoip from 'geoip-lite';
-
-export default function handler(req, res) {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const geo = geoip.lookup(ip);
-    const city = geo ? geo.city : 'unknown';
-    res.status(200).json({ city });
-}
