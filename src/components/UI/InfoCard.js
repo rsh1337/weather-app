31da4fb4c2f6ec5518cb1ce515ba56f6f2a4a040
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-04-18 22:37:37 +0300

    Fetch panel with API, add animations, and make it responsive

--- src/components/UI/InfoCard.js
+++ src/components/UI/InfoCard.js
@@ -1,18 +1,136 @@
 import Image from 'next/image';
-import { Popover } from '@headlessui/react';
-
-function Panel() {
+import {Popover, Transition} from '@headlessui/react';
+import useSWR from "swr";
+import {fetcher} from "../../../lib/fetcher";
+function Panel({index, latitude, longitude}) {
+	const { data, error } = useSWR(`/api/7dayPanel?lat=${latitude}&long=${longitude}`,fetcher);
+	if (error) {
+		return(
+			<div
+				className={
+					'absolute top-0 translate-x-2 md:translate-x-80 -ml-2 md:ml-3 backdrop-blur-md bg-[#E2EAFC]/30 z-30 rounded-xl w-full'
+				}
+			>
+				<Popover.Button className={"p-3 md:hidden block focus:outline-0 focus:outline"}>
+					<Image src={"/Icons/x.svg"} alt={'X icon'} height={15} width={15} />
+				</Popover.Button>
+				<h1 className={"font-light text-center mb-2 md:mt-2 mt-0"}>Error Fetching Data</h1>
+			</div>
+		)
+	}
+	if (!data) {
+		return(
+			<div
+				className={
+					'absolute top-0 translate-x-2 md:translate-x-80 -ml-2 md:ml-3 backdrop-blur-md bg-[#E2EAFC]/30 z-30 rounded-xl w-full'
+				}
+			>
+				<Popover.Button className={"p-3 md:hidden block focus:outline-0 focus:outline"}>
+					<Image src={"/Icons/x.svg"} alt={'X icon'} height={15} width={15} />
+				</Popover.Button>
+				<h1 className={"font-light text-center mb-2 md:mt-2 mt-0 backdrop-blur-lg shadow-md rounded-md animate-pulse w-[8rem] ml-[6rem] h-5"}></h1>
+				<div className={'grid grid-cols-3 gap-2 p-1 font-light text-sm '}>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+					<div
+						className={'flex flex-col p-2 justify-center items-center backdrop-blur-lg shadow-md rounded-md animate-pulse'}
+					>
+						<div
+							className={
+								'flex flex-col gap-3 justify-center items-center h-3'
+							}
+						>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+	console.log(data)
+	const unixTimeMs = `${data.daily.sunrise[index]}` * 1000;
+	const date = new Date(unixTimeMs);
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const formattedTime = `${hours}:${minutes}`;
 	return (
 		<div
 			className={
-				'fixed top-0 -ml-2 md:ml-3 backdrop-blur-md bg-[#E2EAFC]/30 z-30 rounded-xl w-full'
+				'absolute -top-16 overflow-y-auto translate-x-2 md:translate-x-80 -ml-2 md:ml-3 backdrop-blur-md bg-[#E2EAFC]/30 z-30 rounded-xl w-full'
 			}
 		>
-			<Popover.Button className={"p-3 md:hidden block"}>
+			<Popover.Button className={"p-3 md:hidden block focus:outline-0 focus:outline"}>
 				<Image src={"/Icons/x.svg"} alt={'X icon'} height={15} width={15} />
 			</Popover.Button>
-			<h1 className={"font-light text-center mb-2 md:mt-2 mt-0"}>Tuesday</h1>
-			<div className={'grid grid-cols-3 gap-2 p-1 font-light text-sm'}>
+			<h1 className={"font-light text-center mb-2 md:mt-2 mt-0"}>{data.daily.time[index]}</h1>
+			<div className={'grid grid-cols-3 gap-2 p-1 font-light text-sm mb-2'}>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
 				>
@@ -29,7 +147,7 @@ function Panel() {
 						/>
 						<h1 className={'capitalize'}>Sun Rise</h1>
 					</div>
-					<h1>06:19</h1>
+					<h1>{formattedTime}</h1>
 				</div>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
@@ -45,9 +163,9 @@ function Panel() {
 							width={30}
 							height={30}
 						/>
-						<h1 className={'capitalize'}>Humidity</h1>
+						<h1 className={'capitalize'}>Rain</h1>
 					</div>
-					<h1>69%</h1>
+					<h1>{data.daily.rain_sum[index]} mm</h1>
 				</div>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
@@ -65,7 +183,7 @@ function Panel() {
 						/>
 						<h1 className={'capitalize'}>Wind</h1>
 					</div>
-					<h1>69 km/h</h1>
+					<h1>{data.daily.windspeed_10m_max[index]} km/h</h1>
 				</div>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
@@ -83,7 +201,7 @@ function Panel() {
 						/>
 						<h1 className={'capitalize'}>Uv Index</h1>
 					</div>
-					<h1>69</h1>
+					<h1>{data.daily.uv_index_max[index]}</h1>
 				</div>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
@@ -101,7 +219,7 @@ function Panel() {
 						/>
 						<h1 className={'capitalize'}>Precipitation</h1>
 					</div>
-					<h1>69 %</h1>
+					<h1>{data.daily.precipitation_probability_max[index]} %</h1>
 				</div>
 				<div
 					className={'flex flex-col p-2 justify-center items-center'}
@@ -112,50 +230,14 @@ function Panel() {
 						}
 					>
 						<Image
-							src={'/weather-icons/temperature.svg'}
+							src={'/weather-icons/temperature-cold.svg'}
 							alt={'temperature icon'}
-							width={30}
-							height={30}
+							width={15}
+							height={15}
 						/>
-						<h1 className={'capitalize text-center'}>Feeling Temperature</h1>
+						<h1 className={'capitalize text-center'}>Min Feeling Temperature</h1>
 					</div>
-					<h1>69 °C</h1>
-				</div>
-				<div
-					className={'flex flex-col p-2 justify-center items-center'}
-				>
-					<div
-						className={
-							'flex flex-col gap-3 justify-center items-center'
-						}
-					>
-						<Image
-							src={'/weather-icons/2.svg'}
-							alt={'temperature icon'}
-							width={30}
-							height={30}
-						/>
-						<h1 className={'capitalize text-center'}>Cloud Cover</h1>
-					</div>
-					<h1>69 %</h1>
-				</div>
-				<div
-					className={'flex flex-col p-2 justify-center items-center'}
-				>
-					<div
-						className={
-							'flex flex-col gap-3 justify-center items-center'
-						}
-					>
-						<Image
-							src={'/weather-icons/eye.svg'}
-							alt={'Eye icon'}
-							width={30}
-							height={30}
-						/>
-						<h1 className={'capitalize text-center'}>Visibility</h1>
-					</div>
-					<h1>69 m</h1>
+					<h1>{data.daily.apparent_temperature_min[index]} °C</h1>
 				</div>
 			</div>
 		</div>
@@ -174,10 +256,10 @@ export default function InfoCard({
 
 	return (
 		<Popover>
-			<div className={'flex flex-col md:flex-row'}>
+			<div className={'relative'}>
 				<Popover.Button
 					className={
-						'flex flex-row w-full items-center justify-between p-3 rounded-2xl'
+						'flex flex-row w-full items-center justify-between p-3 rounded-2xl focus:outline-0 focus:outline'
 					}
 				>
 					<div className={'flex flex-row items-center gap-2'}>
@@ -194,9 +276,18 @@ export default function InfoCard({
 						<h1>Max: {tempMax}°C</h1>
 					</div>
 				</Popover.Button>
-				<Popover.Panel>
-					<Panel/>
+				<Transition
+					enter="transition duration-100 ease-out"
+					enterFrom="transform scale-95 opacity-0"
+					enterTo="transform scale-100 opacity-100"
+					leave="transition duration-75 ease-out"
+					leaveFrom="transform scale-100 opacity-100"
+					leaveTo="transform scale-95 opacity-0"
+				>
+				<Popover.Panel className={"relative top-0"}>
+					<Panel latitude={latitude} longitude={longitude} index={index}/>
 				</Popover.Panel>
+				</Transition>
 			</div>
 		</Popover>
 	);
---
+++ src/pages/api/7dayPanel.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+    const {long, lat} = req.query
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=apparent_temperature_max,apparent_temperature_min,sunrise,uv_index_max,precipitation_sum,rain_sum,precipitation_probability_max,windspeed_10m_max&timeformat=unixtime&timezone=auto`);
+    const data = await response.json();
+    if (data.error){
+        return res.status(400).json({message: data.reason, error: true});
+    }
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const formattedDates = data.daily.time.map(date => {
+        const weekday = weekdays[new Date(date * 1000).getDay()];
+        return `${weekday}`;
+    });
+    const formattedData = {
+        ...data,
+        daily: {
+            ...data.daily,
+            time: formattedDates,
+        },
+    };
+
+    return res.status(200).json(formattedData);
+}
